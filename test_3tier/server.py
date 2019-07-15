66a444c9df575914506695bff6f02b9f402616ff
jzide <jzide@10.109.210.184>
2019-07-15 21:46:48 +0900

    update

--- test_3tier/server.py
+++ test_3tier/server.py
@@ -4,10 +4,19 @@ Created on 2019. 7. 15.
 @author: jzide
 '''
 
+import os
+import random
 import pygics
 
+def workloader(count):
+    data = 0
+    for i in range(0, count): data = data + random.randrange(1,3)
+    exit(0)
+
 @pygics.rest('GET', '/')
-def test(req, *args):
-    return {'result' : 'OK'}
+def test(req, count=10000, *args):
+    pid = os.fork()
+    if pid == 0: workloader(count)
+    return {'pid' : str(pid)}
 
 pygics.server('0.0.0.0', 8080)
