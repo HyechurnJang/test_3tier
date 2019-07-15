34f7ce31da8a42048f921e764bb4ccabd975f6a3
jzide <jzide@10.109.210.184>
2019-07-15 21:56:18 +0900

    update

--- test_3tier/server.py
+++ test_3tier/server.py
@@ -5,19 +5,25 @@ Created on 2019. 7. 15.
 '''
 
 import os
+import time
 import random
 import pygics
 
-def workloader(count):
-    data = 0
-    for i in range(0, count): data = data + random.randrange(1,3)
+def workloader(sec):
+    stt = time.time()
+    while True:
+        data = 0
+        for i in range(0, 10000):
+            data = data + random.randrange(1,3)
+        end = time.time()
+        if end - stt > sec: break
     exit(0)
 
 @pygics.rest('GET', '/')
-def test(req, count=10000, *args):
-    count = int(count)
+def test(req, sec=1, *args):
+    sec = int(sec)
     pid = os.fork()
-    if pid == 0: workloader(count)
+    if pid == 0: workloader(sec)
     return {'pid' : str(pid)}
 
 pygics.server('0.0.0.0', 8080)
